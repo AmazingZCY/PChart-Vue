df57f41698681f5e615ae0af20b053deee0fca3e
Your Name <you@example.com>
2019-07-24 21:45:34 +0800

    添加数据验证

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,17 +1,44 @@
 <template>
   <div id="wrap">
     <div v-show="!isSigin" id="log">
-    <div><h1>PChat</h1></div>
+      <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="账号" prop="uid">
+    <el-input v-model.number="ruleForm.uid"></el-input>
+  </el-form-item>
+  <el-form-item label="密码" prop="pass">
+    <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="login" id="login">登录</el-button>
+    <el-button @click="gotoSigin"  id="sigin">注册</el-button>
+  </el-form-item>
+</el-form>
+    <!-- <div><h1>PChat</h1></div>
     <div><label class="tit">账号：</label><div class="text"><el-input v-model="uid" placeholder="请输入账号"></el-input></div></div>
     <div><label class="tit">密码：</label><div class="text"><el-input v-model="password" placeholder="请输入密码" show-password></el-input></div></div>
-    <div><el-button type="primary" id="login" @click="login">登录</el-button><el-button type="primary" id="sigin" @click="gotoSigin">注册</el-button></div>
+    <div><el-button type="primary" id="login" @click="login">登录</el-button><el-button type="primary" id="sigin" @click="gotoSigin">注册</el-button></div> -->
     </div>
     <div v-show="isSigin" id="sig">
-      <div><h1>PChat</h1></div>
+      <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+            <el-form-item label="账号" prop="uid">
+    <el-input v-model.number="ruleForm.uid"></el-input>
+  </el-form-item>
+  <el-form-item label="密码" prop="pass">
+    <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+  </el-form-item>
+  <el-form-item label="确认密码" prop="checkPass">
+    <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="gotoLogin"  id="login">登录</el-button>
+    <el-button @click="sigin"  id="sigin">注册</el-button>
+  </el-form-item>
+</el-form>
+      <!-- <div><h1>PChat</h1></div>
     <div><label class="tit">账号：</label><div class="text"><el-input v-model="uid" placeholder="请输入账号"></el-input></div></div>
     <div><label class="tit">密码：</label><div class="text"><el-input v-model="password" placeholder="请输入密码" show-password></el-input></div></div>
     <div><label class="tit">确认密码：</label><div class="text"><el-input v-model="password0" placeholder="请再次输入密码" show-password></el-input></div></div>
-    <div><el-button type="primary" id="login" @click="gotoLogin">登录</el-button><el-button type="primary" id="sigin" @click="sigin">注册</el-button></div>
+    <div><el-button type="primary" id="login" @click="gotoLogin">登录</el-button><el-button type="primary" id="sigin" @click="sigin">注册</el-button></div> -->
     </div>
   </div>
 </template>
@@ -20,37 +47,76 @@
 export default {
   name: 'sigin',
   data () {
-    return {
-      uid:'',
-      password:'',
-      password0:'',
-      isSigin: false
-    }
+    // return {
+    //   uid:'',
+    //   password:'',
+    //   password0:'',
+    //   isSigin: false
+    // }
+     var checkUid = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('账号不能为空'));
+        };
+        if(value.length>12){
+          return callback(new Error('账号长度过长！'))
+        }
+
+      };
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.ruleForm.pass) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
+      return {
+        ruleForm: {
+          pass: '',
+          checkPass: '',
+          uid: '',
+        },
+        isSigin: false,
+        rules: {
+          pass: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+          checkPass: [
+            { validator: validatePass2, trigger: 'blur' }
+          ],
+          uid: [
+            { validator: checkUid, trigger: 'blur' }
+          ]
+        }
+      };
   },
 
   methods: {
           login () {
             var _this = this
-          if(this.uid==""){
-              this.message('账号未输入！');
-              return
-          }
-          if(this.password==""){
-              this.message('密码未输入！');
-              return
-          }
           this.ajax({
               method:'get',
-              url:'http://localhost:3030/login?uid='+this.uid+'&password='+this.password
+              url:'http://localhost:3030/login?uid='+this.ruleForm.uid+'&password='+this.ruleForm.pass
           }).then(function(res){
             console.log(res)
               if(res.data.code == 202){
                 console.log(res.data.code)
-                _this.$socket.emit('login',_this.uid)
-                _this.$store.state.user = _this.uid
-                _this.uid = '';
-                _this.password = '';
-                _this.password0 = '';
+                _this.$socket.emit('login',_this.ruleForm.uid)
+                _this.$store.state.user = _this.ruleForm.uid
+                _this.ruleForm.uid = '';
+                _this.ruleForm.pass = '';
+                _this.ruleForm.checkPass = '';
               }else if(res.data.code == 302){
                 _this.postMsg(res.data.Msg)
               }else {
@@ -62,32 +128,15 @@ export default {
       },
       sigin () {
         var _this = this
-          if(this.uid==""){
-              this.message('账号未输入！');
-              return
-          }
-          if(this.password==""){
-              this.message('密码未输入！');
-              return
-          }
-          if(this.password0==""){
-              this.message('密码未再次输入！');
-              return
-          }
-          if(this.password !==this.password0){
-              this.message('两次输入的密码不一致！');
-              this.password0 = ''
-              return
-          }
           this.ajax({
               method:'get',
-              url:'http://localhost:3030/sigin?uid='+this.uid+'&password='+this.password
+              url:'http://localhost:3030/sigin?uid='+this.ruleForm.uid+'&password='+this.ruleForm.pass
           }).then(function(res){
               if(res.data.code == 201){
                 _this.postMsg(res.data.Msg);
-                _this.uid = '';
-                _this.password = '';
-                _this.password0 = '';
+                _this.ruleForm.uid = '';
+                _this.ruleForm.pass = '';
+                _this.ruleForm.checkPass = '';
                 _this.isSigin = false;
               }else if(res.data.code == 301){
                 _this.postMsg(res.data.Msg);
@@ -99,15 +148,15 @@ export default {
           })
       },
       gotoSigin () {
-        this.uid = '';
-        this.password = '';
+        this.ruleForm.uid = '';
+        this.ruleForm.pass = '';
                 
         this.isSigin = true;
       },
       gotoLogin () {
-        this.uid = '';
-        this.password = '';
-        this.password0 = '';
+        this.ruleForm.uid = '';
+        this.ruleForm.pass = '';
+        this.ruleForm.checkPass = '';
         this.isSigin = false;
       },
       postMsg(msg) {
@@ -135,14 +184,15 @@ export default {
 </script>
 <style scoped>
     #wrap{
-        width: 500px;
+        width: 400px;
         height: 300px;
         position: absolute;
         left: 50%;
         top: 40%;
-        margin: -150px 0 0 -250px;
+        margin: -177px 0 0 -225px;
         border: 2px solid #409eff;
         border-radius: 5px;
+        padding: 50px 50px 0 0;
     }
     h1{
       width: 100px;
@@ -162,16 +212,14 @@ export default {
       display: inline-block;
     }
     #login,#sigin{
-      font-size: 28px;
-      position: absolute;
-      bottom: 30px;
-      left: 50%;
+      font-size: 18px;
+      /* position: absolute;
+      bottom: -100px;
+      left: 50%; */
       width: 100px;
       height: 50px;
     }
-    #login{
-      margin-left: -150px;
-    }
+
     #sigin{
       margin-left: 50px;
     }
